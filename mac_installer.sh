d9d126897cfb63d49ff5acfca52f106cda173fe6
P3TCH <petchuser@outlook.com>
2023-03-17 15:39:25 +0700

    kafka install for mac

---
+++ mac_installer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+printf "Checking Homebrew...\n"
+
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+	printf "\n##### You don't have Homebrew, Installing...\n"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+	printf "\n##### Homebrew has installed. Updating...\n"
+    brew update
+fi
+
+printf "\n##### Installing kafka...\n"
+brew install kafka
+printf "\n##### Starting kafka service...\n"
+brew services start kafka
+printf "\n##### Starting zookeeper service...\n"
+brew services start zookeeper
+printf "\n##### DONE.. Installer by.P3TCH\n"
+
+printf "\n\n##### kafka config location is : /opt/homebrew/Cellar/kafka/3.4.0/libexec/config\n"
+printf "##### zookeeper config location is : /opt/homebrew/Cellar/zookeeper/3.8.1/libexec/zookeeper-contrib/zookeeper-contrib-zkpython/src/test"
