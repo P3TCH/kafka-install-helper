fd31def31a3d57580d01485929430fe5d24964ce
P3TCH <petchuser@outlook.com>
2023-03-17 16:11:51 +0700

    ubuntu installer done.

--- ubuntu_installer.sh
+++ ubuntu_installer.sh
@@ -17,13 +17,37 @@ wget https://downloads.apache.org/kafka/3.4.0/kafka_2.13-3.4.0.tgz
 tar -xvzf kafka_2.13-3.4.0.tgz --strip 1
 
 cd ~/kafka/config
+curl https://raw.githubusercontent.com/P3TCH/kafka-install-helper/main/ubuntu-config/server.properties -o server.properties
 
+sudo cd /etc/systemd/system/
+sudo curl https://raw.githubusercontent.com/P3TCH/kafka-install-helper/main/ubuntu-config/zookeeper.service -o /etc/ststemd/system/zookeeper.service
+sudo curl https://raw.githubusercontent.com/P3TCH/kafka-install-helper/main/ubuntu-config/kafka.service -o /etc/ststemd/system/kafka.service
 
-printf "\n##### Starting zookeeper...\n"
-bin/zookeeper-server-start.sh config/zookeeper.properties &
+printf "\n##### Starting service...\n"
+sudo systemctl daemon-reload
+sudo systemctl start zookeeper
+sudo systemctl start kafka
+sudo systemctl enable zookeeper
+sudo systemctl enable kafka
 
-printf "\n##### Starting kafka...\n"
-bin/kafka-server-start.sh config/server.properties &
+printf "\n##### Setting command...\n"
+sudo echo "alias kafka-topics='~/kafka/bin/kafka-topics.sh'" >> ~/.bashrc
+sudo echo "alias kafka-console-producer='~/kafka/bin/kafka-console-producer.sh'" >> ~/.bashrc
+sudo echo "alias kafka-console-consumer='~/kafka/bin/kafka-console-consumer.sh'" >> ~/.bashrc
+sudo echo "alias kafka-server-start='~/kafka/bin/kafka-server-start.sh'" >> ~/.bashrc
+sudo echo "alias kafka-server-stop='~/kafka/bin/kafka-server-stop.sh'" >> ~/.bashrc
+sudo echo "alias kafka-consumer-groups='~/kafka/bin/kafka-consumer-groups.sh'" >> ~/.bashrc
+sudo echo "alias kafka-configs='~/kafka/bin/kafka-configs.sh'" >> ~/.bashrc
+sudo echo "alias kafka-acls='~/kafka/bin/kafka-acls.sh'" >> ~/.bashrc
+sudo echo "alias kafka-reassign-partitions='~/kafka/bin/kafka-reassign-partitions.sh'" >> ~/.bashrc
+sudo echo "alias kafka-preferred-replica-election='~/kafka/bin/kafka-preferred-replica-election.sh'" >> ~/.bashrc
+sudo echo "alias kafka-log-dirs='~/kafka/bin/kafka-log-dirs.sh'" >> ~/.bashrc
+sudo echo "alias zookeeper-server-start='~/kafka/bin/zookeeper-server-start.sh'" >> ~/.bashrc
+sudo echo "alias zookeeper-server-stop='~/kafka/bin/zookeeper-server-stop.sh'" >> ~/.bashrc
+
+printf "\n##### Done!\n"
+
+source ~./bashrc
 
 #printf "\n##### Creating topic...\n"
 #bin/kafka-topics.sh --create --topic test --bootstrap-server localhost:9092
